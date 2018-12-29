d14b37789d1c373ed4714b4169de8708433ff932
matt0605 <szjava@ymail.com>
2018-12-29 17:37:51 -0500

    add Quiz3

---
+++ src/quiz/Quiz3.java
@@ -0,0 +1,24 @@
+package quiz;
+
+/*
+What is the output of this code:
+
+for (int i=1;i<5;i++) {
+ System.out.print(i);
+ }
+ Output:
+ 1234
+ 
+ */
+
+
+public class Quiz3 {
+
+	public static void main(String[] args) {
+
+		for (int i = 1; i < 5; i++) {
+
+			System.out.print(i);
+		}
+	}
+}
